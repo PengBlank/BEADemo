2d24a2b7ea4a2972a16113afab807b02f83fa325
chibipd <925137520@qq.com>
2015-11-26 14:46:22 +0800

    for next step,add page control's page

---
+++ BEA_Demo/BankDashBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  BankDashBoardViewController.h
+//  BEA_Demo
+//
+//  Created by Dict on 15/11/24.
+//  Copyright © 2015年 Dict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BankDashBoardViewController : UIViewController
+
+@end
---
+++ BEA_Demo/BankDashBoardViewController.m
@@ -0,0 +1,37 @@
+//
+//  BankDashBoardViewController.m
+//  BEA_Demo
+//
+//  Created by Dict on 15/11/24.
+//  Copyright © 2015年 Dict. All rights reserved.
+//
+
+#import "BankDashBoardViewController.h"
+
+@interface BankDashBoardViewController ()
+
+@end
+
+@implementation BankDashBoardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
